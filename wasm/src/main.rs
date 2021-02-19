3344a2ff757798d8805731ec010234849b8e2311
qianchao <qianch@eccjt.com>
2021-02-19 17:42:01 +0800

    添加wasm项目

--- wasm/src/main.rs
+++ wasm/src/main.rs
@@ -1,4 +1,25 @@
+use std::hash::Hash;
+
+mod graph;
 fn main() {
-    println!("Hello, world!");
+    test_compare_point();
 }
+
+fn test_distance_between_point(){
+    let p1=graph::Point::new(11, 12);
+    let p2=graph::Point{
+        x:22,
+        y:56
+    };
+    let dist=graph::distance_between_point(p1, p2);
+    println!("{}",dist);
+}
+
+fn test_compare_point(){
+    let p1=graph::Point::new(11, 13);
+    let p2=graph::Point::new(11, 12);
+    println!("{}",);
+    println!("{}",graph::compare_point(p1, p2));
+}
+
 
